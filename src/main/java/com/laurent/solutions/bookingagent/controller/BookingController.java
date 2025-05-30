f56d53dad20bd59116b769f8b01f809977e3ab40
ulaurent <ulaurent12@gmail.com>
2025-05-29 20:26:19 -0400

    controller definitions

---
+++ src/main/java/com/laurent/solutions/bookingagent/controller/BookingController.java
@@ -0,0 +1,30 @@
+package com.laurent.solutions.bookingagent.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/booking")
+public class BookingController {
+    @GetMapping("/status")
+    public String status() {
+        return "Reservation is confirmed";
+    }
+
+    @GetMapping("/details")
+    public String details() {
+        return "Reservation details: [Date: 2023-10-01, Time: 18:00, Guests: 2]";
+    }
+
+    @PostMapping("/cancel")
+    public String cancel() {
+        return "Reservation has been cancelled";
+    }
+
+    @PostMapping("/book")
+    public String book() {
+        return "Reservation has been booked successfully";
+    }
+}
