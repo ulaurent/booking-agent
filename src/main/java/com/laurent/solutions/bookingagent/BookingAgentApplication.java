f840f1b56971e2787cb07b588ccfbfbbae9d14cc
ulaurent <ulaurent12@gmail.com>
2025-05-29 20:03:57 -0400

    initial commit

---
+++ src/main/java/com/laurent/solutions/bookingagent/BookingAgentApplication.java
@@ -0,0 +1,13 @@
+package com.laurent.solutions.bookingagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laurent/solutions/bookingagent/BookingAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurent.solutions.bookingagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
